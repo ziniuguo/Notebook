83d690d196b08a4e1eaa27fccbf61ec500f7ff28
Guo Ziniu <51998550+ziniuguo@users.noreply.github.com>
2021-06-25 19:17:32 +0800

    Add files via upload

---
+++ styles/basic.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: "#eaeaea"
+    },
+    title: {
+        marginTop: 100,
+        paddingVertical: 8,
+        borderWidth: 2,
+        borderColor: "#20232a",
+        borderRadius: 6,
+        backgroundColor: "#61dafb",
+        color: "#20232a",
+        textAlign: "center",
+        fontSize: 25,
+        fontWeight: "normal"
+    },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
+    },
+});
