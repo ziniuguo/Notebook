ed7bf0c95285d01756316890f7aa1db0169c6cfa
Guo Ziniu <51998550+ziniuguo@users.noreply.github.com>
2021-06-29 00:40:34 +0800

    Update basic.js

--- styles/basic.js
+++ styles/basic.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { StyleSheet } from "react-native";
+import {StyleSheet} from "react-native";
 
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 24,
-        backgroundColor: "#eaeaea"
-    },
-    title: {
-        marginTop: 100,
-        paddingVertical: 8,
-        borderWidth: 2,
-        borderColor: "#20232a",
-        borderRadius: 6,
-        backgroundColor: "#61dafb",
-        color: "#20232a",
-        textAlign: "center",
-        fontSize: 25,
-        fontWeight: "normal"
-    },
-    button: {
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#DDDDDD",
-        padding: 10
-    },
-});
+export const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            paddingTop: 70,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "flex-start",
+        },
+        title: {
+            paddingVertical: 8,
+            borderWidth: 2,
+            borderColor: "#20232a",
+            borderRadius: 6,
+            backgroundColor: "#61dafb",
+            color: "#20232a",
+            textAlign: "center",
+            fontSize: 25,
+            fontWeight: "normal"
+        },
+        button: {
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#d0d0d0",
+            padding: 10
+        },
+    }
+);
