8cad708346908bed87d0772d6d07fb1024ec49f8
Guo Ziniu <51998550+ziniuguo@users.noreply.github.com>
2021-06-30 15:30:18 +0800

    Update Notes.js

--- screens/Notes.js
+++ screens/Notes.js
@@ -2,8 +2,8 @@ import React from "react";
 import {Alert, ScrollView, Text, ToastAndroid, TouchableOpacity, View} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {createStackNavigator} from "react-navigation-stack";
-import {styles} from "../styles/basic";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import {styles} from "../styles/basic";
 
 class notesScreen extends React.Component {
     static navigationOptions = {
@@ -13,19 +13,9 @@ class notesScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            st: false,
             list: [],
             placeholder: 'Nothing is here...'
         };
-    }
-
-    saveCheckState = async (btnState) => {
-        try {
-            this.setState({st: btnState});
-            await AsyncStorage.setItem('btn', JSON.stringify(btnState))
-        } catch (e) {
-            // save error
-        }
     }
 
     getStringValue = async () => {
@@ -35,33 +25,32 @@ class notesScreen extends React.Component {
                     ? JSON.parse(await AsyncStorage.getItem("key"))
                     : []
             console.log(await AsyncStorage.getItem("key"))
+            console.log(itemGet)
             this.setState({
                 list: itemGet,
                 placeholder: itemGet.length === 0 ? 'Nothing is here...' : '',
-                st: await AsyncStorage.getItem("btn") === 'true'
             })
-            console.log(this.state.list)
         } catch (e) {
             // save error
         }
     };
 
-    removeValue() {
+    removeValue(i) {
         Alert.alert(
-            'Delete notes you save',
-            'You can not recover any content after this action.',
+            'Delete this note',
+            'Delete this note? You can not recover this note after this action.',
             [
                 {
                     text: 'Delete',
                     onPress: async () => {
                         try {
-                            await AsyncStorage.removeItem("key");
-                            await AsyncStorage.removeItem('btn')
+                            const tempVal = this.state.list;
+                            tempVal.splice(i);
+                            await AsyncStorage.setItem('key', JSON.stringify(tempVal))
                             this.setState(
                                 {
-                                    list: [],
-                                    st: false,
-                                    placeholder: 'Nothing is here...'
+                                    list: tempVal,
+                                    placeholder: tempVal.length === 0 ? 'Nothing is here...' : '',
                                 }
                             );
                             ToastAndroid.show(
@@ -80,6 +69,41 @@ class notesScreen extends React.Component {
         )
     }
 
+    removeAll() {
+        this.state.list.length === 0
+            ? ToastAndroid.show("Nothing here can be deleted...", ToastAndroid.SHORT)
+            : Alert.alert(
+            'Delete All',
+            'Delete all the notes you saved?',
+            [
+                {
+                    text: 'Delete',
+                    onPress: async () => {
+                        await AsyncStorage.removeItem('key');
+                        this.setState({
+                            list: [],
+                            placeholder: 'Nothing is here...'
+                        });
+                        ToastAndroid.show(
+                            "All notes deleted.",
+                            ToastAndroid.SHORT)
+                    }
+                },
+                {
+                    text: 'Cancel'
+                }
+            ],
+            {cancelable: false}
+            )
+    }
+
+    saveCheckedState = async (el, ind) => {
+        const ori = this.state.list;
+        ori[ind] = el.replace(/.$/, el.slice(-1) === '0' ? '1' : '0')
+        this.setState({list: ori})
+        await AsyncStorage.setItem('key', JSON.stringify(ori))
+    }
+
     componentDidMount() {
         this._unsubscribe = this.props.navigation.addListener(
             "willFocus",
@@ -96,9 +120,9 @@ class notesScreen extends React.Component {
             <View style={{
                 paddingTop: 5,
                 paddingBottom: 20,
-                paddingHorizontal: 30,
                 flex: 1,
-                justifyContent: 'space-between'
+                justifyContent: 'space-between',
+                paddingHorizontal: 30
             }}>
                 <Text
                     style={{alignSelf: 'center'}}
@@ -108,31 +132,25 @@ class notesScreen extends React.Component {
                 <ScrollView>
                     {this.state.list.map((item, index) => (
                         <BouncyCheckbox
+                            style={{paddingRight: 30, paddingVertical: 5}}
                             key={index}
-                            style={{
-                                padding: 5
-                            }}
                             fillColor="#61dafb"
                             iconStyle={{borderColor: "#61dafb"}}
                             textContainerStyle={{marginLeft: 10}}
-                            text={item}
-                            isChecked={this.state.st}
+                            text={item.slice(0, -1)}
+                            isChecked={item.slice(-1) !== '0'}
                             disableBuiltInState={true}
-                            onPress={() => this.saveCheckState(!this.state.st)}
+                            onPress={() => this.saveCheckedState(item, index)}
+                            onLongPress={() => this.removeValue(index)}
                         />
                     ))}
                 </ScrollView>
-                <View>
-                    <Text>{'state is : ' + this.state.st}</Text>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => {
-                            this.removeValue()
-                        }}
-                    >
-                        <Text>Delete</Text>
-                    </TouchableOpacity>
-                </View>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => this.removeAll()}
+                >
+                    <Text>Delete All</Text>
+                </TouchableOpacity>
             </View>
         );
     }
