06af29e3f218f0b6595d22689fddf50924af3b3e
Guo Ziniu <51998550+ziniuguo@users.noreply.github.com>
2021-07-02 01:04:09 +0800

    Update Details.js

--- screens/Details.js
+++ screens/Details.js
@@ -1,36 +1,21 @@
 import React from "react";
-import {Image, ScrollView, Text, View} from "react-native";
+import {SafeAreaView, Text} from "react-native";
 import {createStackNavigator} from "react-navigation-stack";
-
-const logo = {
-    uri: "https://reactnative.dev/img/tiny_logo.png",
-    width: 64,
-    height: 64,
-};
+import Br from "../tags";
 
 class detailsScreen extends React.Component {
     static navigationOptions = {
         title: "About",
+        safeAreaInsets: {top: 0}
     };
 
     render() {
         return (
-            <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-                <Text style={{fontSize: 35}}>Note.Notes.Test</Text>
-                <ScrollView>
-                    <Text style={{fontSize: 96}}>Scroll me plz</Text>
-                    <Image source={logo}/>
-                    <Text style={{fontSize: 96}}>If you like</Text>
-                    <Image source={logo}/>
-                    <Text style={{fontSize: 96}}>Scrolling down</Text>
-                    <Image source={logo}/>
-                    <Text style={{fontSize: 96}}>What's the best</Text>
-                    <Image source={logo}/>
-                    <Text style={{fontSize: 96}}>Framework around?</Text>
-                    <Image source={logo}/>
-                    <Text style={{fontSize: 80}}>React Native</Text>
-                </ScrollView>
-            </View>
+            <SafeAreaView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+                <Text style={{fontSize: 35}}>A Notebook</Text>
+                <Br/>
+                <Text>Written in React Native</Text>
+            </SafeAreaView>
         );
     }
 }
